26fdd6d917cfaacdddf125615349bab48d2618cc
HA i forgot my name <31477538+Chasinggoodgrades@users.noreply.github.com>
2018-05-30 09:35:14 -0400

    Add files via upload

--- space-war-1-.py
+++ space-war-1-.py
@@ -3,10 +3,10 @@ import pygame
 import random
 import sys
 import pygame.sprite as sprite
+import xbox360_controller
 
 # Initialize game engine
 pygame.init()
-
 
 # Window
 WIDTH = 900
@@ -61,21 +61,25 @@ mob_img = pygame.image.load('images/mob.png')
 bomb_img = pygame.image.load('images/bluelaser.png')
 spacegod = pygame.image.load('images/spacegod.png')
 spacebg = pygame.image.load('images/background.gif')
-healthbar_img = pygame.image.load("images/healthbar.png")
-health_img = pygame.image.load("images/health.png")
+healthbar_img = pygame.image.load('images/healthbar.png')
+health_img = pygame.image.load('images/health.png')
+startscreen = pygame.image.load('images/startscreen.png')
 
 
 # Sounds
-#EXPLOSION = pygame.mixer.Sound('sounds/explosion.ogg')
-
-# Game classes
-# Game classes
-#def draw(self)
-
-#draw_shield_bar(self.screen, 50, 50, player.shield)
+explosion = pygame.mixer.Sound('sounds/explosion.wav')
+shoot_s = pygame.mixer.Sound('sounds/shoot.wav')
+background = pygame.mixer.Sound('sounds/background.wav')
 
 
+# Xbox Controller
+##my_controller = xbox360_controller.Controller(0)
+##
+##LEFT = pygame.K_LEFT
+##RIGHT = pygame.K_RIGHT
+##SHOOT = pygame.K_SPACE
 
+# Game Sprite Classes
 class Ship(pygame.sprite.Sprite):
     def __init__(self, x, y, image):
         super().__init__()
@@ -108,6 +112,7 @@ class Ship(pygame.sprite.Sprite):
         laser.rect.centerx = self.rect.centerx
         laser.rect.centery = self.rect.top
         lasers.add(laser)
+        shoot_s.play()
 
     def update(self, bombs):
         hit_list = pygame.sprite.spritecollide(self, bombs, True)
@@ -116,12 +121,18 @@ class Ship(pygame.sprite.Sprite):
             # play hit sound
             self.shield -= 25
 
+        #Screen Collision
+        if self.rect.x >= WIDTH - 96:
+            self.rect.x =  WIDTH- 96
+        elif self.rect.x <= 0:
+            self.rect.x = 0
+
         hit_list = pygame.sprite.spritecollide(self, mobs, False)
         if len(hit_list) > 0:
             self.shield = 0
 
         if self.shield == 0:
-            #EXPLOSION.play()
+            explosion.play()
             self.kill()
 
         
@@ -163,6 +174,16 @@ class Mob(pygame.sprite.Sprite):
         bombs.add(bomb)
     
     def update(self, lasers, player):
+        global score
+
+##        location = (self.rect.x, self.rect.y - 40, self.shield / 3, 15)
+##        location2 = (self.rect.x - 1, self.rect.y - 41, (255 / 3) + 2, 17)
+##        color = (-1 * (self.shield - 255), self.shield, 0)
+##
+##        pygame.draw.rect(screen, BLACK, location2)
+##        pygame.draw.rect(screen, color, location)
+
+        #Check if Hit
         hit_list = pygame.sprite.spritecollide(self, lasers, True, pygame.sprite.collide_mask)
 
         if len(hit_list) > 0:
@@ -171,6 +192,7 @@ class Mob(pygame.sprite.Sprite):
         elif self.shield < 0:
             player.score += 1
             self.kill()
+            explosion.play()
 
 
 
@@ -232,48 +254,86 @@ class Fleet:
             bomber.drop_bomb()
 
 def setup():
-    global ship, mobs, stage, player, bombs, lasers, fleet
+    global ship, mobs, stage, player, bombs, lasers, fleet, wave, score
 
+    pygame.mixer.music.load("sounds/background.wav")
+    pygame.mixer.music.play(-1)
+    
     ship = Ship(384, 536, ship_img)
-    mob1 = Mob(128, 64, mob_img)
-    mob2 = Mob(256, 64, mob_img)
-    mob3 = Mob(384, 64, mob_img)
-    mob4 = Mob(128, 0, mob_img)
-    mob5 = Mob(256, 0, mob_img)
-    mob6 = Mob(384, 0, mob_img)
-
+    
     player = pygame.sprite.GroupSingle()
     player.add(ship)
     player.score = 0
-
-
-    lasers = pygame.sprite.Group()
-
-    mobs = pygame.sprite.Group()
-    mobs.add(mob1, mob2, mob3, mob4, mob5, mob6)
-
-    bombs = pygame.sprite.Group()
-
-
-    fleet = Fleet(mobs)
+    
+    player.score = 0
+    wave = 1
+    score = 0
 
     stage = START
 
-# set stage
+def start(wave):
+    global ship, mobs, stage, player, bombs, lasers, fleet, score
 
+    if score!= 0:
+        wavestart.play()
 
-    
-# Make game objects
+    if wave == 1 or wave == 2:
+        mob1 = Mob(128, 64, mob_img)
+        mob2 = Mob(256, 64, mob_img)
+        mob3 = Mob(384, 64, mob_img)
+        
+        mobs = pygame.sprite.Group()
+        mobs.add(mob1,mob2,mob3)
+        
+    elif wave == 3 or wave == 4:
+        mob1 = Mob(128, 64, mob_img)
+        mob2 = Mob(256, 64, mob_img)
+        mob3 = Mob(384, 64, mob_img)
+        mob4 = Mob(256, 180, mob_img)
+        
+        mobs = pygame.sprite.Group()
+        mobs.add(mob1,mob2,mob3,mob4)
+        fleet.speed += 0.25
+        
+    elif wave == 5 or wave == 6:
+        mob1 = Mob(128, 64, mob_img)
+        mob2 = Mob(256, 64, mob_img)
+        mob3 = Mob(384, 64, mob_img)
+        mob4 = Mob(192, 180, mob_img)
+        mob5 = Mob(320, 180, mob_img)
+        
+        mobs = pygame.sprite.Group()
+        mobs.add(mob1,mob2,mob3,mob4,mob5)
 
+    else:
+        mob1 = Mob(128, 64, mob_img)
+        mob2 = Mob(256, 64, mob_img)
+        mob3 = Mob(384, 64, mob_img)
+        mob4 = Mob(192, 180, mob_img)
+        mob5 = Mob(320, 180, mob_img)
+        
+        
+        mobs = pygame.sprite.Group()
+        mobs.add(mob1,mob2,mob3,mob4,mob5)
+        fleet.speed += 0.25
 # Make sprite groups
+
+    lasers = pygame.sprite.Group()
+
+    bombs = pygame.sprite.Group()
+
+    fleet = Fleet(mobs)
 
 #Game Helper Functions
 def show_title_screen():
     title_text = FONT_XL.render("Space War!", 1, WHITE)
-    screen.blit(title_text, [128, 204])
+    space_to_play = FONT_MD.render("Press Space to Play!!", 1, WHITE)
+    screen.blit(startscreen, (0, 1))
+    screen.blit(title_text, [120, 204])
+    screen.blit(space_to_play, [350, 350])
 
 def show_stats(player):
-    score_text = FONT_MD.render(str(player.score), 1, WHITE)
+    score_text = FONT_MD.render("Score: " + str(player.score), 1, WHITE)
     screen.blit(score_text, [32, 32])
 
 def show_lose():
@@ -286,11 +346,20 @@ def show_win():
 
 def restart_screen():
     text = FONT_MD.render("Press R to play again!", 1, WHITE)
-    screen.blit(text, [0, 100])
+    screen.blit(text, [350, 350])
+    stage = START
+
+def draw_wave():
+    wave_text = FONT_MD.render("Wave: " + str(wave), 1, WHITE)
+    screen.blit(wave_text, [32, 50])
+
+    
+    
 
 # Game loop
 setup()
 done = False
+start(wave)
 
 while not done:
     # Event processing (React to key presses, mouse clicks, etc.)
@@ -304,7 +373,7 @@ while not done:
             elif stage == PLAYING:
                 if event.key == pygame.K_SPACE:
                     ship.shoot()
-            elif restart == 1:
+            elif stage == END:
                 if event.key == pygame.K_r:
                     setup()
 
@@ -331,8 +400,11 @@ while not done:
     if stage == PLAYING:
         if stage == PLAYING:
             if len(mobs) == 0:
-                stage = END
+                wave += 1
+                start(wave)
             elif ship.shield == 0:
+                stage = END
+            elif wave >= 10:
                 stage = END
 
     # Drawing code (Describe the picture. It isn't actually drawn yet.)
@@ -351,6 +423,7 @@ while not done:
     bombs.draw(screen)
     mobs.draw(screen)
     show_stats(player)
+    draw_wave()
     ship.h_bar()
 
     if stage == START:
@@ -365,6 +438,9 @@ while not done:
             show_lose()
             restart_screen()
             restart = 1
+        elif wave >= 10:
+            show_win()
+            restart_screen()
 
 
     # Update screen (Actually draw the picture in the window.)
